2c03daed2e9bb414d26363675d4f76303862218c
liyunwen <344795118@qq.com>
2018-11-26 12:03:14 +0800

    dockerbuild的shell初始化

---
+++ docker/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+version=$1
+
+if [ -z "$version" ]; then
+    echo '请输入版本号' 
+    exit
+fi
+
+echo "构建镜像 $version 开始"
+
+echo "step1: 构建本地镜像"
+docker build -t 19931106/swoft:$version --build-arg CACHEBUST=$(date +%s) .
+
+echo "step2: 关联到远程仓库"
+docker tag 19931106/swoft:$version https://hub.docker.com/r/19931106/swoft:$version
+
+echo "step3: 推送到远程仓库"
+docker push https://hub.docker.com/r/19931106/swoft:$version
+
+echo "镜像构建完成，可以拉取新的镜像使用了"
