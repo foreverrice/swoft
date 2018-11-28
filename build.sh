e5801efe0c1b16a76adb3fd31ea983bf1a3214b2
liyunwen <344795118@qq.com>
2018-11-28 19:31:58 +0800

    build.sh推送的问题

--- build.sh
+++ build.sh
@@ -15,5 +15,7 @@ echo "step2: 关联到远程仓库"
 docker tag 19931106/swoft:$version 19931106/swoft:$version
 
 echo "step3: 推送到远程仓库"
-docker push https://hub.docker.com/r/19931106/swoft:$version
+#仓库地址：https://hub.docker.com/r/19931106/swoft
+#我用的免费的dockerhub，要先登录docker login https://hub.docker.com
+docker push 19931106/swoft:$version
 echo "镜像构建完成，可以拉取新的镜像使用了"
